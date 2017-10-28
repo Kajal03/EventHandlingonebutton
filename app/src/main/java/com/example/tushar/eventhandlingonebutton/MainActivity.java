6c3e9cc15dfb469d6449d5702ae25fe2606d7b59
Kajal <connected.kajal@gmail.com>
2017-10-28 13:48:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tushar/eventhandlingonebutton/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.tushar.eventhandlingonebutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button A = (Button) findViewById(R.id.button);
+
+        A.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View v)
+                    {
+                        TextView B = (TextView) findViewById(R.id.textView);
+                        B.setText("Bye !!");
+                    }
+                }
+        );
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        TextView B = (TextView) findViewById(R.id.textView);
+        B.setText("Hi !!");
+    }
+}
